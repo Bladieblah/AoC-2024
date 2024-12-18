dfc1132e1f90458866bf5128a4f7146ef1d614d9
Matthijs Wesseling <matthijswesseling@gmail.com>
2024-12-18 12:35:02 +0100

    Binary search

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,4 +1,3 @@
-use hashbrown::HashMap;
 use itertools::Itertools;
 
 fn next(pos: (usize, usize), dir: usize) -> (usize, usize) {
@@ -61,38 +60,10 @@ fn step(
   result
 }
 
-fn find_paths(
-  paths: &mut Vec<Vec<bool>>,
-  cache: &Vec<Vec<usize>>,
-  pos: (usize, usize),
-  dir: usize,
-  total: usize,
-  size: usize
-) {
-  vec![dir, l(dir), r(dir)]
-    .iter()
-    .for_each(|next_dir| {
-      let next_pos = next(pos, *next_dir);
-      if
-        next_pos.0 < size &&
-        next_pos.1 < size &&
-        !paths[next_pos.0][next_pos.1] &&
-        cache[next_pos.0][next_pos.1] == total - 1
-      {
-        paths[next_pos.0][next_pos.1] = true;
-        find_paths(paths, cache, next_pos, *next_dir, total - 1, size);
-      }
-    });
-}
-
-fn get_dist_map(cache: &Vec<Vec<usize>>) -> HashMap<usize,usize> {
-  cache
-    .iter()
-    .flat_map(|x| x)
-    .group_by(|x| **x)
-    .into_iter()
-    .map(|(k, v)| (k, v.count()))
-    .collect()
+fn fill(grid: &mut Vec<Vec<bool>>, bytes: &Vec<(usize,usize)>, start: usize, end: usize) {
+  for k in start..end {
+    grid[bytes[k].1][bytes[k].0] = true;
+  }
 }
 
 #[aoc24::main(18)]
@@ -107,44 +78,29 @@ fn main(input: &str) -> (usize, String) {
   let size = 71;
   let bytes_init = 1024;
   let mut grid = vec![vec![false; size]; size];
+  fill(&mut grid, &bytes, 0, bytes_init);
   let mut cache = vec![vec![0_usize; size]; size];
-  cache[0][0] = 1;
-  for k in 0..bytes_init {
-    grid[bytes[k].1][bytes[k].0] = true;
-  }
-  // for line in grid.iter() {
-  //   for c in line {
-  //     print!("{}", if *c {'x'} else {'.'});
-  //   }
-  //   println!();
-  // }
-  let mut length = step(&grid, &mut cache, size, (0, 0), 1, 1);
-  let p1 = length.unwrap() - 1;
-  let mut byte_count = bytes_init;
-  let mut paths = vec![vec![false; size]; size];
-  find_paths(&mut paths, &cache, (size - 1, size - 1), 3, length.unwrap(), size);
-  let mut dists = get_dist_map(&cache);
+  let p1 = step(&grid, &mut cache, size, (0, 0), 1, 1).unwrap() - 1;
 
-  while length.is_some() && byte_count < bytes.len() {
-    let (c, r) = bytes[byte_count];
-    grid[r][c] = true;
-    byte_count += 1;
-    print!("({:?}, {}) {} -> ", (c,r), cache[r][c], dists[&cache[r][c]]);
-    *dists.get_key_value_mut(&cache[r][c]).unwrap().1 -= 1;
-    println!("{}", dists[&cache[r][c]]);
-    if dists[&cache[r][c]] == 0 {
-      println!("Regenerating at byte count {}", byte_count);
-      cache = vec![vec![0_usize; size]; size];
-      cache[0][0] = 1;
-      length = step(&grid, &mut cache, size, (0, 0), 1, 1);
-      if length.is_some() {
-        paths = vec![vec![false; size]; size];
-        find_paths(&mut paths, &cache, (size - 1, size - 1), 3, length.unwrap(), size);
-        dists = get_dist_map(&cache);
+  let mut lower = bytes_init;
+  let mut upper = bytes.len();
+
+  while upper - lower != 1 {
+    let mid = (upper + lower) / 2;
+    let mut new_grid = grid.clone();
+    fill(&mut new_grid, &bytes, lower, mid);
+    cache = vec![vec![0_usize; size]; size];
+    match step(&new_grid, &mut cache, size, (0, 0), 1, 1) {
+      Some(_) => {
+        lower = mid;
+        grid = new_grid;
+      }
+      None => {
+        upper = mid;
       }
     }
   }
 
-  let (c, r) = bytes[byte_count];
+  let (c, r) = bytes[lower];
   (p1, format!("{},{}", c, r))
 }
